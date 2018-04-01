a508952f1f9fad8314172543bf4bbe0df54d0500
kaleonorman <kaleonorman@gmail.com>
2018-03-31 19:02:12 -1000

    updated decryption to return to client

--- Server.py
+++ Server.py
@@ -1,4 +1,40 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
+import base64
+import hashlib
+
+from Crypto import Random
+from Crypto.Cipher import AES
+
+class AESCipher(object):
+    """
+    A classical AES Cipher. Can use any size of data and any size of password thanks to padding.
+    Also ensure the coherence and the type of the data with a unicode to byte converter.
+    """
+    def __init__(self, key):
+        self.bs = 32
+        self.key = hashlib.sha256(AESCipher.str_to_bytes(key)).digest()
+
+    @staticmethod
+    def str_to_bytes(data):
+        u_type = type(b''.decode('utf8'))
+        if isinstance(data, u_type):
+            return data.encode('utf8')
+        return data
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+
+
 
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
@@ -14,25 +50,30 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         # Send message back
         message = "Hello"
 
-        self.wfile.write(bytes(message, "utf8"))
+        self.wfile.write(bytes(message.encode()))
         return
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
+        new_cipher = AESCipher(key='mykey')
+        decrypted = new_cipher.decrypt(body)
+        print(decrypted)
+        message = bytes(decrypted, 'utf-8')
         self.send_response(200)
         self.end_headers()
         response = BytesIO()
         response.write(b'This is POST request. ')
         response.write(b'Received: ')
-        response.write(body)
+        response.write(message)
+     #   response.write(decrypted)
         self.wfile.write(response.getvalue())
 
 
 
 def run():
     print('starting server...')
-    server_address = ('', 8000)
+    server_address = ('localhost', 8000)
     httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
     print('running server...')
     httpd.serve_forever()
